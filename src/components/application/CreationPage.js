30b644bfa6a23aa64d0008876b702f87c990fc83
Gregory Frommelt <71351990+fromgreg@users.noreply.github.com>
2021-04-10 14:15:41 +0200

    added resources; fixed Select behaviour of game modes

--- src/components/application/CreationPage.js
+++ src/components/application/CreationPage.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import styled, {ThemeProvider} from 'styled-components';
+import styled from 'styled-components';
 import {BackgroundContainer} from "../../helpers/layout";
 import {Button} from "../../views/design/Button";
 import jassboard from "../../views/images/interfaceImages/jasstafel.svg";
-import Select, { components } from 'react-select';
-import makeAnimated from 'react-select/animated';
+import Select from 'react-select';
 import acorn from "../../views/images/icons/acorn.png";
 import rose from "../../views/images/icons/rose.png";
 import bell from "../../views/images/icons/bell.png";
 import shield from "../../views/images/icons/shield.png";
+import undenufe from "../../views/images/icons/undenufe.png";
+import obenabe from "../../views/images/icons/obenabe.png";
+import slalom from "../../views/images/icons/slalom.png";
+import gusti from "../../views/images/icons/gusti.png";
+import mary from "../../views/images/icons/mary.png";
 
 
 
-const optionsIngameMode = [
+let optionsIngameMode = [
   { value: 'acorn', label: <div><img src={acorn} height={'20px'} width={'20px'}/>Acorn</div>, multiplicator: 1 },
   { value: 'rose', label: <div><img src={rose} height={'20px'} width={'20px'}/>Rose</div>, multiplicator: 1 },
-  { value: 'bell', label: <div><img src={bell} height={'20px'} width={'20px'}/>Bell</div>, multiplicator: 2 },
-  { value: 'shield', label: <div><img src={shield} height={'20px'} width={'20px'}/>Shield</div>, multiplicator: 2 }
+  { value: 'bell', label: <div><img src={bell} height={'20px'} width={'20px'}/>Bell</div>, multiplicator: 1 },
+  { value: 'shield', label: <div><img src={shield} height={'20px'} width={'20px'}/>Shield</div>, multiplicator: 1 },
+  { value: 'undenufe', label: <div><img src={undenufe} height={'20px'} width={'20px'}/>Undenufe</div>, multiplicator: 1 },
+  { value: 'obenabe', label: <div><img src={obenabe} height={'20px'} width={'20px'}/>Obenabe</div>, multiplicator: 1 },
+  { value: 'slalom', label: <div><img src={slalom} height={'20px'} width={'20px'}/>Slalom</div>, multiplicator: 1 },
+  { value: 'gusti', label: <div><img src={gusti} height={'20px'} width={'20px'}/>Gusti</div>, multiplicator: 1 },
+  { value: 'mary', label: <div><img src={mary} height={'20px'} width={'20px'}/>Mary</div>, multiplicator: 1 },
 ];
 
 const optionsMultiplicators = [
@@ -31,6 +40,26 @@ const optionsMultiplicators = [
   { value: 9, label: '9x'},
   { value: 10, label: '10x'},
 ];
+
+const customColumnStyle = {
+  multiValue: provided => ({
+    ...provided,
+    width: '100%',
+    height: '33px'
+  }),
+  multiValueLabel: provided => ({
+    ...provided,
+    marginRight: 'auto',  // push delete icon to the far right
+  }),
+  input: provided => ({
+    ...provided,
+    display: 'flex',
+    alignItems: 'flex-start',
+  }),
+  valueContainer: (provided, state) => ({
+    ...provided,
+  })
+};
 
 const JassboardContainer = styled('div')({
   display: 'flex',
@@ -57,12 +86,14 @@ const Container = styled('div')({
 
 const InnerContainer = styled('div')({
   display: 'flex',
-  flexDirection: 'column',
+  flexDirection: props => props.flexDirection || 'column',
   justifyContent: 'top',
   width: props => props.width || '50%',
   height: '100%',
   borderRight: props => props.borderRight || null,
-  borderLeft: props => props.borderLeft || null
+  borderLeft: props => props.borderLeft || null,
+  marginLeft: props => props.marginLeft || null,
+  marginRight: props => props.marginRight || null
 });
 
 const TextContainer = styled('div')({
@@ -87,27 +118,38 @@ export default class CreationPage extends React.Component {
       weis: false,
       crossWeis: null,
       weisAsk: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+  handleChange = (selector, event) => {
+    switch (selector) {
+      case 'multiplicators': {
+        const updatedIngameModes = this.state.ingameModes.map(m =>
+          m === event.mode ? {...event.mode, multiplicator: event.newMult.value} : m
+        );
+        this.setState({ingameModes: updatedIngameModes});
+        break;
+      };
+      case 'modes': {
+        this.setState({ingameModes: event});
+        break;
+      };
     }
-  };
+  }
 
-  handleIngameModeChange = modes => {
-    this.setState({ingameModes: modes});
-    console.log(this);
-  };
-
-  handleMultiplicatorChange = (mode, value) => {
-    const updatedIngameModes = this.state.ingameModes.map(m => {
-      if(m === mode) {
-        return {...mode, multiplicator: value.value};
-      }
-      return m;
-    });
-    this.setState({ingameModes: updatedIngameModes});
-    console.log(this);
-  };
 
   componentDidMount() {
-    this.setState({ ingameModes: optionsIngameMode});
+    this.setState({
+      ingameModes: [
+          optionsIngameMode[0],
+          optionsIngameMode[1],
+          optionsIngameMode[2],
+          optionsIngameMode[3],
+          optionsIngameMode[4],
+          optionsIngameMode[5]
+      ]
+    });
   };
 
   render() {
@@ -118,29 +160,41 @@ export default class CreationPage extends React.Component {
             <TextContainer>Topbar</TextContainer>
           </Container>
           <Container height={'70%'}>
-
-            <InnerContainer width={'30%'}>
-              <TextContainer>Ingame Modes</TextContainer>
-              <Select
-                onChange={value => this.handleIngameModeChange(value)}
-                closeMenuOnSelect={false}
-                components={makeAnimated()}
-                options={optionsIngameMode}
-                defaultValue={optionsIngameMode}
-                noOptionsMessage={() => "No game modes selected"}
-                isMulti
-                autoFocus
-              />
-            </InnerContainer>
-
-            <InnerContainer width={'20%'}>
-              {this.state.ingameModes?.map(mode =>
+            <InnerContainer>
+              <TextContainer>
+                Ingame Modes
+              </TextContainer>
+              <InnerContainer width={'100%'} flexDirection={'row'}>
+                <InnerContainer width={'70%'}>
                   <Select
-                    options={optionsMultiplicators}
-                    defaultValue={optionsMultiplicators[mode.multiplicator - 1]}
-                    onChange={value => this.handleMultiplicatorChange(mode, value)}
+                      styles={customColumnStyle}
+                      onChange={newModes => this.handleChange('modes', newModes)}
+                      closeMenuOnSelect={false}
+                      options={optionsIngameMode}
+                      defaultValue={[
+                        optionsIngameMode[0],
+                        optionsIngameMode[1],
+                        optionsIngameMode[2],
+                        optionsIngameMode[3],
+                        optionsIngameMode[4],
+                        optionsIngameMode[5]
+                      ]}
+                      noOptionsMessage={() => "No game modes selected"}
+                      isMulti
+                      autoFocus
                   />
-              )}
+                </InnerContainer>
+                <InnerContainer width={'30%'} marginRight={'15px'}>
+                  {this.state.ingameModes?.map(mode =>
+                      <Select
+                          key={mode.value}
+                          options={optionsMultiplicators}
+                          defaultValue={optionsMultiplicators[0]}
+                          onChange={newMult => this.handleChange('multiplicators', {mode, newMult})}
+                      />
+                  )}
+                </InnerContainer>
+              </InnerContainer>
             </InnerContainer>
 
             <InnerContainer borderLeft={'1px solid #9A9899'}>
